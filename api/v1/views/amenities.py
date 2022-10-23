255fb981d5b6dba3ade17755fc66d1c3fe9eb4d7
cecilia-89 <catabong89@gmail.com>
2022-10-23 15:59:08 +0100

    renamed file

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -6,47 +6,47 @@ from models import storage
 
 
 @app_views.route('/amenities/<amenity_id>',
-				 methods=['GET', 'DELETE', 'PUT'],
-				 strict_slashes=False)
+                 methods=['GET', 'DELETE', 'PUT'],
+                 strict_slashes=False)
 def amenity(amenity_id):
-	"""returns an amenity based on it's id"""
-	for amenity in storage.all(Amenity).values():
-		if amenity.id == amenity_id:
-			if request.method == 'DELETE':
-				amenity.delete()
-				storage.save()
-				return '{}'
+    """returns an amenity based on it's id"""
+    for amenity in storage.all(Amenity).values():
+        if amenity.id == amenity_id:
+            if request.method == 'DELETE':
+                amenity.delete()
+                storage.save()
+                return '{}'
 
-			elif request.method == 'PUT':
-				res = request.get_json()
-				if res is None:
-					abort(400, description='Not a JSON')
-				for k, v in res.items():
-					if k.endswith('ed_at') or k == 'state_id' or k == 'id':
-						continue
-					setattr(amenity, k, v)
-				amenity.save()
+            elif request.method == 'PUT':
+                res = request.get_json()
+                if res is None:
+                    abort(400, description='Not a JSON')
+                for k, v in res.items():
+                    if k.endswith('ed_at') or k == 'state_id' or k == 'id':
+                        continue
+                    setattr(amenity, k, v)
+                amenity.save()
 
-			return jsonify(amenity.to_dict())
+            return jsonify(amenity.to_dict())
 
-	abort(404)
+    abort(404)
 
 
 @app_views.route('/amenities',
-				 methods=['GET', 'POST'],
-				 strict_slashes=False)
+                 methods=['GET', 'POST'],
+                 strict_slashes=False)
 def amenities():
-	"""displays and creates an amenity"""
-	if request.method == 'POST':
-		res = request.get_json()
-		if res is None:
-			abort(400, description='Not a JSON')
-		if 'name' not in res.keys():
-			abort(400, description='Missing name')
+    """displays and creates an amenity"""
+    if request.method == 'POST':
+        res = request.get_json()
+        if res is None:
+            abort(400, description='Not a JSON')
+        if 'name' not in res.keys():
+            abort(400, description='Missing name')
 
-		new_amenity = Amenity(**res)
-		new_amenity.save()
-		return jsonify(new_amenity.to_dict()), 201
+        new_amenity = Amenity(**res)
+        new_amenity.save()
+        return jsonify(new_amenity.to_dict()), 201
 
-	amenity = [v.to_dict() for v in storage.all(Amenity).values()]
-	return jsonify(amenity)
+    amenity = [v.to_dict() for v in storage.all(Amenity).values()]
+    return jsonify(amenity)
--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -6,51 +6,51 @@ from models import storage
 
 
 @app_views.route('/users/<user_id>',
-				 methods=['GET', 'DELETE', 'PUT'],
-				 strict_slashes=False)
+                 methods=['GET', 'DELETE', 'PUT'],
+                 strict_slashes=False)
 def user(user_id):
-	"""returns an user based on it's id"""
-	for user in storage.all(User).values():
-		if user.id == user_id:
-			if request.method == 'DELETE':
-				user.delete()
-				storage.save()
-				return '{}'
+    """returns an user based on it's id"""
+    for user in storage.all(User).values():
+        if user.id == user_id:
+            if request.method == 'DELETE':
+                user.delete()
+                storage.save()
+                return '{}'
 
-			elif request.method == 'PUT':
-				res = request.get_json()
-				if res is None:
-					abort(400, description='Not a JSON')
-				for k, v in res.items():
-					if k.endswith('ed_at') or k == 'email' or k == 'id':
-						continue
-					setattr(user, k, v)
-				user.save()
+            elif request.method == 'PUT':
+                res = request.get_json()
+                if res is None:
+                    abort(400, description='Not a JSON')
+                for k, v in res.items():
+                    if k.endswith('ed_at') or k == 'email' or k == 'id':
+                        continue
+                    setattr(user, k, v)
+                user.save()
 
-			return jsonify(user.to_dict())
+            return jsonify(user.to_dict())
 
-	abort(404)
+    abort(404)
 
 
 @app_views.route('/users',
-				 methods=['GET', 'POST'],
-				 strict_slashes=False)
+                 methods=['GET', 'POST'],
+                 strict_slashes=False)
 def users():
-	"""displays and creates an user"""
-	if request.method == 'POST':
-		res = request.get_json()
-		if res is None:
-			abort(400, description='Not a JSON')
-		if 'email' not in res.keys():
-			abort(400, description='Missing email')
+    """displays and creates an user"""
+    if request.method == 'POST':
+        res = request.get_json()
+        if res is None:
+            abort(400, description='Not a JSON')
+        if 'email' not in res.keys():
+            abort(400, description='Missing email')
 
-		if 'password' not in res.keys():
-			abort(400, description='Missing password')
+        if 'password' not in res.keys():
+            abort(400, description='Missing password')
 
 
-		new_user = User(**res)
-		new_user.save()
-		return jsonify(new_user.to_dict()), 201
+        new_user = User(**res)
+        new_user.save()
+        return jsonify(new_user.to_dict()), 201
 
-	user = [v.to_dict() for v in storage.all(User).values()]
-	return jsonify(user)
+    user = [v.to_dict() for v in storage.all(User).values()]
+    return jsonify(user)
