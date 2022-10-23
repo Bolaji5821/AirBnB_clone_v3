2562fa751421b68f8009dd58d01dc4338c1b88a6
cecilia-89 <catabong89@gmail.com>
2022-10-23 18:05:42 +0100

    made files executable

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module app.py: starts the flask app"""
+"""Module index.py: starts the flask app"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
