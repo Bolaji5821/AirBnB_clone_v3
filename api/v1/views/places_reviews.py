da4b03869255dfaa03d5a167feea08d39745df15
cecilia-89 <catabong89@gmail.com>
2022-10-24 07:07:15 +0100

    added new line at EOF

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -13,26 +13,27 @@ from models import storage
                  strict_slashes=False)
 def review(review_id):
     """returns an review based on it's id"""
-    for review in storage.all(Review).values():
-        if review.id == review_id:
-            if request.method == 'DELETE':
-                review.delete()
-                storage.save()
-                return '{}'
+    review = storage.get(Review, review_id)
+    if review is None:
+        abort(404)
 
-            if request.method == 'PUT':
-                res = request.get_json()
-                if res is None:
-                    abort(400, description='Not a JSON')
-                for k, v in res.items():
-                    if k.endswith('ed_at') or k.endswith('id'):
-                        continue
-                    setattr(review, k, v)
-                review.save()
+    if request.method == 'DELETE':
+        review.delete()
+        storage.save()
+        return '{}'
 
-            return jsonify(review.to_dict())
+    if request.method == 'PUT':
+        res = request.get_json()
+        if res is None:
+            abort(400, description='Not a JSON')
+        for k, v in res.items():
+            if k.endswith('ed_at') or k.endswith('id'):
+                continue
+            setattr(review, k, v)
+        review.save()
 
-    abort(404)
+    return jsonify(review.to_dict())
+
 
 
 @app_views.route('/places/<place_id>/reviews',
@@ -40,27 +41,28 @@ def review(review_id):
                  strict_slashes=False)
 def reviews(place_id):
     """displays and creates an review"""
-    for place in storage.all(Place).values():
-        if place_id == place.id:
-            if request.method == 'POST':
-                res = request.get_json()
-                if res is None:
-                    abort(400, description='Not a JSON')
-                if 'text' not in res.keys():
-                    abort(400, description='Missing text')
-                if 'user_id' not in res.keys():
-                    abort(400, description='Missing user_id')
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
 
-                users = [user.id for user in storage.all(User).values()]
-                if res['user_id'] not in users:
-                    abort(404)
-                res['place_id'] = place.id
-                new_review = Review(**res)
-                new_review.save()
-                return jsonify(new_review.to_dict()), 201
+    if request.method == 'POST':
+        res = request.get_json()
+        if res is None:
+            abort(400, description='Not a JSON')
+        if 'text' not in res.keys():
+            abort(400, description='Missing text')
+        if 'user_id' not in res.keys():
+            abort(400, description='Missing user_id')
 
-            review = [v.to_dict() for v in storage.all(Review).values()
-                      if v.place_id == place_id]
-            return jsonify(review)
+        user = storage.get(User, res['user_id'])
+        if user is none:
+            abort(404)
+        res['place_id'] = place.id
+        new_review = Review(**res)
+        new_review.save()
+        return jsonify(new_review.to_dict()), 201
 
-    abort(404)
+    review = [v.to_dict() for v in storage.all(Review).values()
+              if v.place_id == place_id]
+    return jsonify(review)
+
