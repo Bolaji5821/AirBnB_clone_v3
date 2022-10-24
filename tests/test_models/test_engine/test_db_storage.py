c34a7f99f2e23cc7240a97ccd3bb70f2c3e6b989
cecilia-89 <catabong89@gmail.com>
2022-10-24 22:52:53 +0100

    added unittests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -112,4 +112,4 @@ class TestFileStorage(unittest.TestCase):
         storage.new(city)
         storage.save()
         c = storage.count()
-        self.assertEqual(len(storage.all()), c
+        self.assertEqual(len(storage.all()), c)
